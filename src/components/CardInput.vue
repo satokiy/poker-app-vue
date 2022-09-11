4e3cdce3b488aec10b1cc2f8fd78c77795c07da0
satokiy <syamada@netprotections.co.jp>
2022-09-11 22:37:50 +0900

    見た目を整える

---
+++ src/components/CardInput.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-form>
+    <v-text-field
+      v-model="inputCards"
+      label="カードを入力してください"
+      outlined
+      clearable
+      :rules="[required]"
+    ></v-text-field>
+    {{ inputCards }}
+  </v-form>
+</template>
+
+<script lang="ts">
+import { ref } from "@vue/reactivity";
+export default {
+  setup() {
+    const inputCards = ref("");
+    const required = (value: string) => {
+      return !!value || "card is required";
+    };
+
+    return {
+      inputCards,
+      required,
+    };
+  },
+};
+</script>
+
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,58 @@
 <template>
-  <hello-world />
+  <!-- <hello-world /> -->
+  <header-info />
+  <v-container>
+    <v-main>
+      <router-view />
+      <v-row justify="center">
+        <h1>check a poker hand</h1>
+      </v-row>
+      <v-row justify="center" class="input">
+        <v-col cols="6">
+          <card-input></card-input>
+        </v-col>
+      </v-row>
+      <v-row justify="center">
+        <v-col cols="3">
+          <v-btn
+          @click="check"
+          color="cyan lighten-4"
+          block>check!!</v-btn>
+        </v-col>
+      </v-row>
+    </v-main>
+  </v-container>
+  <footer-info></footer-info>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-
-// Components
-import HelloWorld from '../components/HelloWorld.vue';
+import { defineComponent } from "vue";
+import HeaderInfo from "../composables/HeaderInfo.vue";
+import FooterInfo from "../composables/FooterInfo.vue";
+import CardInput from "../components/CardInput.vue";
 
 export default defineComponent({
-  name: 'HomeView',
-
-  components: {
-    HelloWorld,
+  name: "HomeView",
+  setup() {
+    const check = () => {
+      console.log("check");
+    };
+    return {
+      check,
+    };
   },
+  components: {
+    HeaderInfo,
+    FooterInfo,
+    CardInput,
+  }
 });
 </script>
+
+<style lang="scss">
+h1 {
+  border: 1px solid red;
+  margin: 20px;
+  padding: 5px;
+}
+</style>
